855f57ab71e17a7c044a98ce43e4143b035f02b3
Adam Ploof <adamploof@hotmail.com>
2022-07-18 22:54:43 -0400

    broke out draw steps into separate methods, center anchor description text properly

--- SignMaker.py
+++ SignMaker.py
@@ -31,40 +31,66 @@ class SignMaker():
         self.price = price
         self.unit = unit
 
+        self.img = None
+        self.draw = None
+
+        self.producer: str = ''
+        self.location: str = ''
         self.extra_info: str = ''
         self.is_organic: bool = False
         self.is_local: bool = False
+
+    def new_img(self):
+        colors = self.get_colors()
+        self.img = Image.new(mode='RGB', size=(self.width, self.height), color=colors['bg'])
+        self.draw = ImageDraw.Draw(self.img)
     
     def make_sign(self):
-        colors = self.get_colors()
-        img = Image.new(mode='RGB', size=(self.width, self.height), color=colors['bg'])
-        draw = ImageDraw.Draw(img)
+        self.new_img()
+        self.draw_accents()
+        self.draw_description()
+
+    def draw_description(self):
+        # Center of image
+        description_location = (round(self.width / 2), round(self.height / 2))
         description_fnt = ImageFont.truetype('/Library/Fonts/NewYork.ttf', self.font_sizes['description'])
+        self.draw.text(description_location, self.description, fill=self.colors['font']['black'], anchor='ms', font=description_fnt)
+    
+    def draw_price(self):
         price_ftn = ImageFont.truetype('/Library/Fonts/NewYork.ttf', self.font_sizes['price'])
-        extra_info_fnt = ImageFont.truetype('/Library/Fonts/NewYork.ttf', self.font_sizes['extra_info'])
+        pass
+
+    def draw_accent_text(self):
         accent_fnt = ImageFont.truetype('/Library/Fonts/NewYork.ttf', self.font_sizes['accent'])
+        # producer and location
+        pass
 
-        # TODO: get specific on centering text
-        description_location = (round(self.height / 3), round(self.width / 4))
+    def draw_extra_info(self):
+        extra_info_fnt = ImageFont.truetype('/Library/Fonts/NewYork.ttf', self.font_sizes['extra_info'])
+        pass
 
-        draw.text(description_location, self.description, self.colors['font']['black'], description_fnt)
-        draw = self.draw_accents(draw)
-        img.show()
+    def display(self):
+        self.img.show()
 
-    def draw_accents(self, draw: ImageDraw):
+    def draw_accents(self):
         accent_color = self.get_colors()['accent']
 
         # Top accent
         top_accent_x = (0, 0)
         top_accent_y = (self.width + 1, round(self.height / 6))
-        draw.rectangle([top_accent_x, top_accent_y], fill=accent_color)
+        self.draw.rectangle([top_accent_x, top_accent_y], fill=accent_color)
 
         # Bottom accent
         bottom_accent_x = (0, self.height + 1)
         bottom_accent_y = (self.width + 1, round((self.height / 6) * 5))
-        draw.rectangle([bottom_accent_x, bottom_accent_y], fill=accent_color)
+        self.draw.rectangle([bottom_accent_x, bottom_accent_y], fill=accent_color)
 
-        return draw
+    def draw_debug(self):
+        # Vertical middle line
+        self.draw.line([(round(self.width / 2), 0), (round(self.width / 2), self.height)], fill='Blue', width=1)
+
+        # Horizontal middle line
+        self.draw.line([(0, round(self.height / 2)), (self.width, round(self.height / 2))], fill='Blue', width=1)
 
     
     def get_colors(self) -> dict[str, tuple]:
@@ -78,6 +104,8 @@ def main():
     sign = SignMaker('Hot New Product', '$12.99', 'ea')
     sign.is_local = True
     sign.make_sign()
+    sign.draw_debug()
+    sign.display()
 
 if __name__ == '__main__':
     main()
