3715dd8010da76d255b4a47b9584c57e2508bd68
Adam Ploof <adamploof@hotmail.com>
2022-07-23 12:12:21 -0400

    rebrand as poster builder, begin hacking out old sign builder properties and methods

--- PosterBuilder.py
+++ PosterBuilder.py
@@ -1,7 +1,27 @@
+"""
+A poster maker for gigs and events.
+
+Grabs an image or two (or more), does some processing to make an interesting background
+and overlays the following fields to create a poster:
+
+- Event title
+- Artist
+- Venue
+- Event date*
+- Event time*
+- Description
+- Extra info
+- Price
+- Contact email
+- Contact phone
+
+*Provide options for multi day events
+"""
+
 from tkinter import *
 from PIL import Image, ImageDraw, ImageFont
 
-class SignMaker():
+class PosterBuilder():
     colors = {
         'main': {
             'reg': (255, 255, 255),
@@ -17,47 +37,48 @@ class SignMaker():
         }
     }
     font_sizes = {
-        'description': 174,
-        'price': 140,
-        'extra_info': 90,
+        'artist': 174,
+        'venue': 140,
+        'datetime': 90,
         'accent': 62
 
     }
     height = 1080
     width = 1920
 
-    def __init__(self, description: str, price: str, unit: str) -> None:
-        self.description = description
-        self.price = price
-        self.unit = unit
-
+    def __init__(self) -> None:
         self.img = None
         self.draw = None
 
-        self.producer: str = ''
-        self.location: str = ''
+        self.event_title: str = ''
+        self.artist: str = ''
+        self.venue: str = ''
+        self.event_date: str = ''
+        self.event_time: str = ''
+        self.description: str = ''
         self.extra_info: str = ''
-        self.is_organic: bool = False
-        self.is_local: bool = False
+        self.price: str = ''
+        self.contact_email: str = ''
+        self.contact_phone: str = ''
 
     def new_img(self):
         colors = self.get_colors()
         self.img = Image.new(mode='RGB', size=(self.width, self.height), color=colors['bg'])
         self.draw = ImageDraw.Draw(self.img)
     
-    def make_sign(self):
+    def make_poster(self):
         self.new_img()
-        self.draw_accents()
-        self.draw_description()
+        self.draw_venue()
+        self.draw_artist()
 
-    def draw_description(self):
+    def draw_artist(self):
         # Center of image
-        description_location = (round(self.width / 2), round(self.height / 2))
-        description_fnt = ImageFont.truetype('/Library/Fonts/NewYork.ttf', self.font_sizes['description'])
-        self.draw.text(description_location, self.description, fill=self.colors['font']['black'], anchor='ms', font=description_fnt)
+        artist_location = (round(self.width / 2), round(self.height / 2))
+        artist_fnt = ImageFont.truetype('/Library/Fonts/NewYork.ttf', self.font_sizes['artist'])
+        self.draw.text(artist_location, self.artist, fill=self.colors['font']['black'], anchor='ms', font=artist_fnt)
     
     def draw_price(self):
-        price_ftn = ImageFont.truetype('/Library/Fonts/NewYork.ttf', self.font_sizes['price'])
+        price_ftn = ImageFont.truetype('/Library/Fonts/NewYork.ttf', self.font_sizes['venue'])
         pass
 
     def draw_accent_text(self):
@@ -66,13 +87,13 @@ class SignMaker():
         pass
 
     def draw_extra_info(self):
-        extra_info_fnt = ImageFont.truetype('/Library/Fonts/NewYork.ttf', self.font_sizes['extra_info'])
+        extra_info_fnt = ImageFont.truetype('/Library/Fonts/NewYork.ttf', self.font_sizes['datetime'])
         pass
 
     def display(self):
         self.img.show()
 
-    def draw_accents(self):
+    def draw_venue(self):
         accent_color = self.get_colors()['accent']
 
         # Top accent
@@ -101,11 +122,11 @@ class SignMaker():
 
 
 def main():
-    sign = SignMaker('Hot New Product', '$12.99', 'ea')
-    sign.is_local = True
-    sign.make_sign()
-    sign.draw_debug()
-    sign.display()
+    poster = PosterBuilder('Hot New Product', '$12.99', 'ea')
+    poster.is_local = True
+    poster.make_poster()
+    poster.draw_debug()
+    poster.display()
 
 if __name__ == '__main__':
     main()
